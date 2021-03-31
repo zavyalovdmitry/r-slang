76790e077d35a9eb2442058a9bf3b576c972061f
fpastl <75983960+fpastl@users.noreply.github.com>
2021-03-31 22:54:33 +0300

    Revert "fix: fix settings context"
    This reverts commit e9303a4ba51f5cfe9df10ae03b28efdcf99f6f48.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import {
   Switch,
   Route,
@@ -14,76 +14,9 @@ import Audiobattle from './Audiobattle';
 import Sprint from './Sprint';
 import Ourgame from './Ourgame';
 import Auth from './Auth';
-import SettingsContext from './SettingsContext';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      wordTranslate: { value: true, title: 'отоброжать перевод слова' },
-      textTranslate: { value: true, title: 'отоброжать перевод примеров' },
-      deleteWord: { value: true, title: 'отоброжать кнопку "удалить слово"' },
-      hardWord: { value: true, title: 'отоброжать кнопку "тяжёлое слово"' },
-    };
-  }
-
-  changeWordTranslate = () => {
-    this.setState((prevState) => ({
-      wordTranslate: {
-        value: !prevState.wordTranslate.value,
-        title: prevState.wordTranslate.title,
-      },
-    }));
-  }
-
-  changeTextTranslate = () => {
-    this.setState((prevState) => ({
-      textTranslate: {
-        value: !prevState.textTranslate.value,
-        title: prevState.textTranslate.title,
-      },
-    }));
-  }
-
-  changeDeleteWord = () => {
-    this.setState((prevState) => ({
-      deleteWord: {
-        value: !prevState.deleteWord.value,
-        title: prevState.deleteWord.title,
-      },
-    }));
-  }
-
-  changeHardWord = () => {
-    this.setState((prevState) => ({
-      hardWord: {
-        value: !prevState.hardWord.value,
-        title: prevState.hardWord.title,
-      },
-    }));
-  }
-
-  render = () => {
-    console.log('SettingsContext');
-    console.log(SettingsContext);
-    const {
-      wordTranslate, textTranslate, deleteWord, hardWord,
-    } = this.state;
-    wordTranslate.action = this.changeWordTranslate;
-    textTranslate.action = this.changeTextTranslate;
-    deleteWord.action = this.changeDeleteWord;
-    hardWord.action = this.changeHardWord;
-    
-
-    return (
-    <SettingsContext.Provider
-        value={{
-          wordTranslate,
-          textTranslate,
-          deleteWord,
-          hardWord,
-        }}
-      >
+const App = () => (
+  <Fragment>
     <Header/>
     <main>
       <Switch>
@@ -115,8 +48,7 @@ class App extends Component {
       </Switch>
     </main>
     <Footer/>
-  </SettingsContext.Provider>);
-  }
-}
+  </Fragment>
+);
 
 export default App;
