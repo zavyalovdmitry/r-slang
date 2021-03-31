196c3829393ce5cac8143c69ccb97bbb480a5373
fpastl <75983960+fpastl@users.noreply.github.com>
2021-03-31 23:08:31 +0300

    Revert "Revert "fix: fix settings context""
    This reverts commit 76790e077d35a9eb2442058a9bf3b576c972061f.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Component } from 'react';
 import {
   Switch,
   Route,
@@ -14,9 +14,76 @@ import Audiobattle from './Audiobattle';
 import Sprint from './Sprint';
 import Ourgame from './Ourgame';
 import Auth from './Auth';
+import SettingsContext from './SettingsContext';
 
-const App = () => (
-  <Fragment>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      wordTranslate: { value: true, title: 'отоброжать перевод слова' },
+      textTranslate: { value: true, title: 'отоброжать перевод примеров' },
+      deleteWord: { value: true, title: 'отоброжать кнопку "удалить слово"' },
+      hardWord: { value: true, title: 'отоброжать кнопку "тяжёлое слово"' },
+    };
+  }
+
+  changeWordTranslate = () => {
+    this.setState((prevState) => ({
+      wordTranslate: {
+        value: !prevState.wordTranslate.value,
+        title: prevState.wordTranslate.title,
+      },
+    }));
+  }
+
+  changeTextTranslate = () => {
+    this.setState((prevState) => ({
+      textTranslate: {
+        value: !prevState.textTranslate.value,
+        title: prevState.textTranslate.title,
+      },
+    }));
+  }
+
+  changeDeleteWord = () => {
+    this.setState((prevState) => ({
+      deleteWord: {
+        value: !prevState.deleteWord.value,
+        title: prevState.deleteWord.title,
+      },
+    }));
+  }
+
+  changeHardWord = () => {
+    this.setState((prevState) => ({
+      hardWord: {
+        value: !prevState.hardWord.value,
+        title: prevState.hardWord.title,
+      },
+    }));
+  }
+
+  render = () => {
+    console.log('SettingsContext');
+    console.log(SettingsContext);
+    const {
+      wordTranslate, textTranslate, deleteWord, hardWord,
+    } = this.state;
+    wordTranslate.action = this.changeWordTranslate;
+    textTranslate.action = this.changeTextTranslate;
+    deleteWord.action = this.changeDeleteWord;
+    hardWord.action = this.changeHardWord;
+    
+
+    return (
+    <SettingsContext.Provider
+        value={{
+          wordTranslate,
+          textTranslate,
+          deleteWord,
+          hardWord,
+        }}
+      >
     <Header/>
     <main>
       <Switch>
@@ -48,7 +115,8 @@ const App = () => (
       </Switch>
     </main>
     <Footer/>
-  </Fragment>
-);
+  </SettingsContext.Provider>);
+  }
+}
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import 'assets/style.scss';
 import { HashRouter } from 'react-router-dom';
-import SettingsContextProvider from './components/SettingsContext';
 
 ReactDOM.render(
   <HashRouter>
-    <SettingsContextProvider>
       <App />
-    </SettingsContextProvider>
   </HashRouter>,
   document.querySelector('#root'),
 );
